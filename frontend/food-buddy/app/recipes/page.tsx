3fe177790c955bed6848630779b0c67552788169
steventio <101958689+Steven-Tio@users.noreply.github.com>
2023-10-28 23:50:06 -0400

    Add picture to recipe cards

--- frontend/food-buddy/app/recipes/page.tsx
+++ frontend/food-buddy/app/recipes/page.tsx
@@ -5,6 +5,11 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import { TopBar } from "../TopBar";
 import foodbuddy from "../assets/foodbuddy2.jpeg";
+import recipe1 from "../assets/recipe1.webp";
+import recipe2 from "../assets/recipe2.jpeg";
+import recipe3 from "../assets/recipe3.jpeg";
+import recipe4 from "../assets/recipe4.jpeg";
+import recipe5 from "../assets/recipe5.jpeg";
 
 const RecipePage = () => {
   const cardsData = [
@@ -16,16 +21,31 @@ const RecipePage = () => {
         "Step 2: Cook",
         "Step 3: Serve",
       ],
+      image: recipe1, // Import the image and add it here
     },
     {
       title: "Recipe 2",
       description: "Description for Card 2",
       instructions: ["Step 1: Mix", "Step 2: Bake", "Step 3: Enjoy"],
+      image: recipe2, // Import the image and add it here
     },
     {
       title: "Recipe 3",
       description: "Description for Card 3",
       instructions: ["Step 1: Mix", "Step 2: Bake", "Step 3: Enjoy"],
+      image: recipe3, // Import the image and add it here
+    },
+    {
+      title: "Recipe 4",
+      description: "Description for Card 4",
+      instructions: ["Step 1: Mix", "Step 2: Bake", "Step 3: Enjoy"],
+      image: recipe4, // Import the image and add it here
+    },
+    {
+      title: "Recipe 5",
+      description: "Description for Card 5",
+      instructions: ["Step 1: Mix", "Step 2: Bake", "Step 3: Enjoy"],
+      image: recipe5, // Import the image and add it here
     },
     // Add more card objects as needed
   ];
@@ -61,15 +81,27 @@ const RecipePage = () => {
           <Card
             key={index}
             style={{
-              minWidth: "400px",
-              height:
+              width:
                 expandedCard === null
                   ? "200px"
                   : expandedCard === index
                   ? "500px"
                   : "0px",
-              margin: "16px",
+              height:
+                expandedCard === null
+                  ? "300px"
+                  : expandedCard === index
+                  ? "500px"
+                  : "0px",
+              margin:
+                expandedCard === null
+                  ? "16px"
+                  : expandedCard === index
+                  ? "16px"
+                  : "0px",
               cursor: "pointer",
+              backgroundImage: `url(${card.image.src})`,
+              backgroundSize: "cover",
             }}
             onClick={() => handleCardClick(index)}
           >
